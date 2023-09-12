31bbbe5a38806d5ea8616940ee3a069ab91dc761
Samet ALTUNDAG <smtaltundag63@gmail.com>
2023-09-13 00:13:04 +0300

    EF Context tanımlandı.

--- EntityLayer/Concrete/Contact.cs
+++ EntityLayer/Concrete/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,20 @@ namespace EntityLayer.Concrete
 {
     public class Contact
     {
+
+        [Key]
         public int ContactID { get; set; }
+
+        [StringLength(50)]
         public string Username { get; set; }
+
+        [StringLength(50)]
         public string UserMail { get; set; }
+
+        [StringLength(50)]
         public string Subject { get; set; }
+
+//        [StringLength(500)]
         public string Message { get; set; }
     }
 }
