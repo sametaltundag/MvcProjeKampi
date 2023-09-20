7631272d72f90bd37a926403c9e46fdde5af3ecd
Samet ALTUNDAG <smtaltundag63@gmail.com>
2023-09-20 23:29:04 +0300

    Yazar işlemleri tamamlandı.

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı boş olamaz.").MinimumLength(3).WithMessage("Yazar adı en az 3 karakter olmalıdır.").MaximumLength(20).WithMessage("Yazar adı en fazla 20 karakter olmalıdır.");
+
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Kategori açıklaması boş olamaz.").MinimumLength(2).WithMessage("Yazar adı en az 2 karakter olmalıdır.").MaximumLength(50).WithMessage("Kategori adı en fazla 50 karakter olmalıdır.");
+
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Yazar hakkındası boş olamaz.").MinimumLength(10).WithMessage("Yazar hakkındası en az 10 karakter olmalıdır.").MaximumLength(250).WithMessage("Yazar hakkındası en fazla 250 karakter olmalıdır.");
+
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Yazar ünvanı boş geçilemez.").MinimumLength(3).WithMessage("E naz 3 karakter giriniz.").MaximumLength(50).WithMessage("En fazla 50 karakter yazılabilir.");
+        }
+    }
+}
--- MvcProjeKampi/Controllers/WriterController.cs
+++ MvcProjeKampi/Controllers/WriterController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +19,48 @@ namespace MvcProjeKampi.Controllers
         {
             var writersValues = wm.GetList();
             return View(writersValues);
+        }
+
+
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            WriterValidator validations = new WriterValidator();
+            ValidationResult results = validations.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriterAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+                return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult EditWriter(int id)
+        {
+            var writerValue = wm.GetByID(id);
+            return View(writerValue);
+        }
+
+        [HttpPost]
+        public ActionResult EditWriter(Writer p)
+        {
+            wm.WriterUpdate(p);
+            return RedirectToAction("Index");
         }
     }
 }
