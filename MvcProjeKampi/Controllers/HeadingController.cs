ef1008abcf702c74a83ac5609c6f2ea51d2d65ae
Samet ALTUNDAG <smtaltundag63@gmail.com>
2023-10-03 23:59:46 +0300

    Başlık işlemleri güncellendi.

--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -49,5 +49,21 @@ namespace MvcProjeKampi.Controllers
             hm.HeadingAdd(p);
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.VLC = valuecategory;
+
+            var headingvalue = hm.getById(id);
+            return View(headingvalue);
+        }
     }
 }
